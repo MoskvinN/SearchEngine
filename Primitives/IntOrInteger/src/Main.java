51468191f7ecaf5fc4a1e44aa8a39793a492015b
MoskvinN <nekmoskva99@gmail.com>
2022-10-24 20:22:22 +0300

    Completed all tasks of paragraph 7.9

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,6 +4,16 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        for (char i = 'А'; i <= 'я'; i++) {
+            System.out.println(i + "-" + (int) i);
+            if(i == 'Е'){
+                System.out.println(i + "-" + (int) i);
+                System.out.println('Ё' + "-" + (int)'Ё');
+            } else if (i == 'е') {
+                System.out.println(i + "-" + (int) i);
+                System.out.println('ё' + "-" + (int)'ё');
+            }
+        }
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
